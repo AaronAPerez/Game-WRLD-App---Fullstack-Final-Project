4bb0b6e1e69562f62d710e77b29a2912732b135b
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-11-06 18:57:33 -0800

    push1 created new branch

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd9606f8e709ebbe4e4014d96a0b181afa125c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a226f13b63d4abe5ecad7097638a49cb00dac1e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
